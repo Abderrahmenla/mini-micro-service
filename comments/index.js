4718bea5d522b8ffbc9c3e8d9e6673e5da26ab9a
Abderrahmenla <abdo.vic@hotmail.com>
2020-12-07 20:19:29 +0100

    adjustement

--- comments/index.js
+++ comments/index.js
@@ -2,19 +2,26 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
+const {
+    randomBytes
+} = require('crypto');
 app.use(bodyParser.json());
-const commentsByPostId={}
-// add cors 
+const commentsByPostId = {}
+// add cors zzz
 app.use(cors());
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 app.post('/posts/:id/comments', (req, res) => {
     const commentId = randomBytes(7).toString('hex');
-    const { content } = req.body;
+    const {
+        content
+    } = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
+    comments.push({
+        id: commentId,
+        content
+    });
     commentsByPostId[req.params.id] = comments;
     res.status(201).send(comments);
 
