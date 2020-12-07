44a27b12d4be8529ca92a2ea1d52d86e2061463f
Abderrahmenla <abdo.vic@hotmail.com>
2020-12-07 20:20:18 +0100

    fix

--- comments/index.js
+++ comments/index.js
@@ -7,7 +7,7 @@ const {
 } = require('crypto');
 app.use(bodyParser.json());
 const commentsByPostId = {}
-// add cors zzz
+// add corszz
 app.use(cors());
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
