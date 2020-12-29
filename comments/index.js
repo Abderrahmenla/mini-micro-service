e3c09ae0c242867267dec71536d6c80ca7dd5e71
Abderrahmenla <abdo.vic@hotmail.com>
2020-12-29 22:39:40 +0100

    emmiting and receiving

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const {
     randomBytes
 } = require('crypto');
@@ -11,19 +12,29 @@ app.use(cors());
 app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments',async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
-    const {
-        content
-    } = req.body;
+    const {content} = req.body;
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({
         id: commentId,
         content
     });
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId : req.params.idf
+        }
+    })
     commentsByPostId[req.params.id] = comments;
     res.status(201).send(comments);
 
+})
+app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+    res.send({});
 })
 app.listen(4001, () => {
     console.log('listening on 4001');
