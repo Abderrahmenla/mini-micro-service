b048cbc7d31561ac57380565f35f370157476487
Abderrahmenla <abdo.vic@hotmail.com>
2020-12-31 12:22:30 +0100

    adding event bus data store

--- query/index.js
+++ query/index.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 const posts = {};
-
-app.get('/posts', (req, res) => {
-    res.send(posts);
-})
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+const handelEvent = (type,data) => {
     if (type==='PostCreated') {
         const { id, title } = data;
         posts[id]={id,title,comments:[]}
@@ -30,8 +26,21 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
-    res.send({});
+  
+}
+app.get('/posts', (req, res) => {
+    res.send(posts);
+})
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    handelEvent(type, data);
+      res.send({});
 })
 app.listen(4002, () => {
     console.log('Listening on 4002');
+    const res = await axios.get('http://localhost:4005/events');
+    for (const event of res.data) {
+        console.log('Processing event:', event.type);
+        handelEvent(event.type, event.data);
+    }
 })
