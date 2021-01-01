a5f4d02759703df4968ea5a30a07f29b56daed97
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-01 23:17:42 +0100

    UNCOMMENT

--- posts/index.js
+++ posts/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 vonst axios = require('axios');
-const bodyParser = require('body-parser'); // what does bodyparser ? ?
+const bodyParser = require('body-parser'); 
 const { randomBytes } = require('crypto');
 app.use(bodyParser.json());
 const posts = {};
