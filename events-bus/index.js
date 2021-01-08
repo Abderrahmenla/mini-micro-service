8183290cf0cf1909fb06dcb1a187a8011f217367
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-08 17:34:20 +0100

    ingress-nginx

--- events-bus/index.js
+++ events-bus/index.js
@@ -4,20 +4,26 @@ const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+
 const events = [];
 
 app.post('/events', (req, res) => {
-    const event = req.body;
-    events.push(event);
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
-    res.send({ status: 'OK' });
-})
-app.get('/events', (req, res) => {
-    res.send(events);
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://posts-clusterip-srv:4000/events', event);
+  axios.post('http://comments-srv:4001/events', event);
+  axios.post('http://query-srv:4002/events', event);
+  axios.post('http://moderation-srv:4003/events', event);
+
+  res.send({ status: 'OK' });
 });
+
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
 app.listen(4005, () => {
-    console.log("listening on port 4005");
-})
+  console.log('Listening on 4005');
+});
