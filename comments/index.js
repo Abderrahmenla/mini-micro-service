98437e20b786955665309b0a358d44008c70bccc
Abderrahmenla <abdo.vic@hotmail.com>
2021-01-12 23:43:33 +0100

    uncomment

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,9 @@
-// imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
+
 
 const app = express();
 app.use(bodyParser.json());
